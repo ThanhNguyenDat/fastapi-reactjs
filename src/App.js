766681972c5c28f1823212b2edcd49a87f16de43
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-10-16 00:16:06 +0700

    update readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
+import image_resource from "./images/zidane.jpg"
+
 function App() {
   const [url, setUrl] = useState();
   const [image, setImage] = useState();
-  const image_resource = {
-      
+  
+  const image_base = {
+      selectedFile: image_resource, /* `null` is a placeholder for the file that will be uploaded. */
+      sizeW: 1000,
+      sizeH: 1000,
+      url: url,
+      api_data: null
   }
 
   function handleChange(event) {
@@ -13,20 +20,13 @@ function App() {
     setUrl(URL.createObjectURL(event.target.files[0]));    
   }
 
-  // useEffect(() => {
-  //   // call api with dependencies
-
-  // }, [image])
-
   async function onPredict(event) {
     // call api
     const formData = new FormData();
-
     formData.append(
       'image',
       image,
       image.name
-      
     )
 
     const url = "http://127.0.0.1:8000/objectdetection";
@@ -39,9 +39,18 @@ function App() {
     <div className="App">
       <h2>Thành nè</h2>
       <input type="file" onChange={handleChange} />
+      <button onClick={onPredict}>Predict!</button>
+
+      <canvas 
+          id="canvas-img" 
+          width={image_base.sizeW} 
+          height={image_base.sizeH} 
+
+          ref={canvas_img => canvas_img}
+      />
 
       <img src={url} alt="" height="480px"/>
-      <button onClick={onPredict}>Predict!</button>
+      
     </div>
   );
 }
