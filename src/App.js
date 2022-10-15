472a3848975657b1f0e41000552c01089ee096d0
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-10-15 23:57:29 +0700

    upload useState

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
-import React, { Component } from "react";
-import image_resource from "./images/zidane.jpg"
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-class App extends Component { 
-    state = {
-        // Initially, no file is selected
-        selectedFile: image_resource, /* `null` is a placeholder for the file that will be uploaded. */
-        sizeW: 1000,
-        sizeH: 1000,
-        url: null,
-        api_data: null
-    };
+function App() {
+  const [url, setUrl] = useState();
+  const [image, setImage] = useState();
+  const image_resource = {
+      
+  }
 
+  function handleChange(event) {
+    setImage(event.target.files[0]);
+    setUrl(URL.createObjectURL(event.target.files[0]));    
+  }
 
-    componentDidMount() {
-        const ctx = this.canvas_img.getContext("2d");
-        
-        // load image into canvas-img
-        const img = new Image();
-        img.src = this.state.selectedFile;
-        this.setState({
-            sizeH: img.height,
-            sizeW: img.width
-        })
+  // useEffect(() => {
+  //   // call api with dependencies
 
-        // call api
-        var x = 743;
-        var y = 45;
-        var w = 743 + 1149;
-        var h = 45 + 720;
+  // }, [image])
 
-        img.onload = () => {
-            // ctx.drawImage(img, 0, 0, this.canvas_img.width, this.canvas_img.height);
-            ctx.drawImage(img, 0, 0, this.state.sizeW, this.state.sizeH);
-            // draw box
-            ctx.beginPath();
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 1;
-            // ctx.rect(x, y, w, h);
-            
-            ctx.stroke();
-        }   
-         
-    }
+  async function onPredict(event) {
+    // call api
+    const formData = new FormData();
+
+    formData.append(
+      'image',
+      image,
+      image.name
+      
+    )
+
+    const url = "http://127.0.0.1:8000/objectdetection";
+    const result = await axios.post(url, formData);
+    console.log("Results: ", result);
+  }
 
 
-    render() {
-        return (
-        <div className="App">
-            <h1>Hello</h1>
-            <canvas 
-                id="canvas-img" 
-                width={this.state.sizeW} 
-                height={this.state.sizeH} 
-                ref={canvas_img => this.canvas_img = canvas_img}
-            />
-        </div>
+  return (
+    <div className="App">
+      <h2>Thành nè</h2>
+      <input type="file" onChange={handleChange} />
 
-        )
-    }
+      <img src={url} alt="" height="480px"/>
+      <button onClick={onPredict}>Predict!</button>
+    </div>
+  );
 }
 
 export default App;
