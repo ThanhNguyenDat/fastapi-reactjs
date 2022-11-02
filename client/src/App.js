9b93a42acc79f0923a4ac6d744995f0cfce08f5b
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-11-03 01:54:19 +0700

    build header

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,40 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
 import { Fragment } from 'react';
 
 function App() {
-  return (
-    <Router>
-    <div className="App">
-      <nav>
-        <ul>
-        <li>
-            <Link to='/'>HomePage</Link>
-          </li>
-          <li>
-            <Link to='/rasabot'>Chatbot</Link>
-          </li>
-          <li>
-            <Link to='/object_detection'>imageobjectdetection</Link>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
 
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          
-          let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
-          if ( route.layout ) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
+                        const Page = route.component;
 
-          const Page = route.component;
-
-          return <Route 
-                  key={index} 
-                  path={route.path} 
-                  element={
-                    <Layout>
-                      <Page />
-                    </Layout>} />;
-          })}
-      </Routes>
-    </div>
-    </Router>
-  );
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/components/Layout/components/Header/index.js
+++ client/src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '../../../../assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +11,26 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* div Logo */}
-                Header ne
+                <div className={cx('logo')}>
+                       <img src={images.logo} alt="AILab Tools"/>
+                </div>
+
+                <div className={cx('search')}>
+                    <input placeholder='Search account or name task label' spellCheck={false}/>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <button className={cx('loading')} >
+                        <FontAwesomeIcon icon={faSpinner}/>
+                        </button>      
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
+
             </div>
         </header>
     );
